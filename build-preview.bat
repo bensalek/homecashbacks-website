21f1e8ade2274ef008f87116ab335e10ed5078cb
Ben Salek <57572429+bensalek@users.noreply.github.com>
2026-05-15 10:46:15 -0400

    revert aback up

--- build-preview.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-E:
-cd "Startup Business in Canada\10th idea - Home buying April 2026\Website\homecashbacks-website"
-echo Building site...
-node build.js
-echo.
-echo Starting preview server...
-start http://localhost:3000
-serve dist --listen 3000
